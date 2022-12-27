b8f667e8143ee1540861208b517af24a87c27659
The Excited Tech <theexcitedtech@gmail.com>
2022-12-27 14:00:01 -0500

    Functional script to backup save files.

--- backup.sh
+++ backup.sh
@@ -1,14 +1,18 @@
 #/bin/bash
-
+clear
 #Script to check to see if there are .sav files. Then backs them up. 
+LOG_FILE='/roms2/backupsavs/backupsavs.log'
 
-cd /roms2
-if [ ! -d backupsavs ] then;
-    mkdir backupsavs
+if [ ! -d "roms2/backupsavs" ]; then
+    sudo mkdir -v /roms2/backupsavs
+    sudo mkdir -v /roms2/backupsavs/states
 fi
 
-echo "Finding .sav files and copying them to backupsavs..."
-find /roms2 -name '*.sav' -exec cp /roms2/backupsavs {} \;
-echo "Finding .state files and copying them to backupsavs..."
-find /roms2 -name '*.state*' -exec cp /roms2/backupsavs/states {} \;
-echo "All done"
+printf "\n\nFinding .sav files and copying them to backupsavs..."
+sudo find /roms2 -name '*.srm' -exec cp -t /roms2/backupsavs/ {} \;
+printf "\n\nFinding .state files and copying them to backupsavs..."
+sudo find /roms2 -name '*.state*' -exec cp -t /roms2/backupsavs/states/ {} \;
+printf "\n\n\e[32mThe backup has been completed"
+sleep 5
+
+exit 0
