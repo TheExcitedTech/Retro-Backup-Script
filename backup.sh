fbb3906d9ae74dfcd04b35aa20cd137c1be90402
The Excited Tech <theexcitedtech@gmail.com>
2022-12-29 14:51:43 -0500

    Added array instead of entering the same find command Repetitively. Trying to keep this script neat and tidy.

--- backup.sh
+++ backup.sh
@@ -1,27 +1,29 @@
 #/bin/bash
-clear
+
 #Script to check to see if there are Save files. Then backs them up. 
 #LOG_FILE='/roms2/backupsavs/backupsavs.log'
-SAVE_TYPES=(".srm" ".state*" ".sav")
+SAVE_TYPES=("srm" "state*" "sav" "mcd")
+
+printf "\033[0;31mThis will overwrite any saves in the backupsav\n"
+printf "\033[0mBacking up save files...\n"
 
 if [ ! -d "roms2/backupsavs" ]; then
     sudo mkdir -v /roms2/backupsavs
 fi
 
 for svfile in ${SAVE_TYPES[@]}; do #creates subdirectories for each file type. 
-    if [ ! -d "roms2/backupsavs/@svfile" ]; then
-    sudo mkdir -v /roms2/backupsavs/@svfile
+    if [ $svfile == 'state*' ]; then #I don't think * will play nice with folder name. 
+        if [ ! -d "/roms2/backupsavs/state" ]; then
+            sudo mkdir -v /roms2/backupsavs/state
+        fi
+        sudo find /roms2 -name "*.$svfile" -exec cp -t /roms2/backupsavs/state {} \;
+    elif [ ! -d "roms2/backupsavs/$svfile" ]; then
+        sudo mkdir -v /roms2/backupsavs/$svfile
     fi
-printf "\n\nFinding @svfile files and copying them to backupsavs..."
-sudo find /roms2 -name "*@svfile" -exec cp -t /roms2/backupsavs/@svfile {} \;
+printf "\n\nFinding $svfile files and copying them to backupsavs..."
+sudo find /roms2 -name "*.$svfile" -exec cp -t /roms2/backupsavs/$svfile {} \;
 done
 
+ printf "\n\n\e[32mYour saves gave been backed up"
 
-# printf "\n\nFinding @svfile files and copying them to backupsavs..."  Isn't needed with the for loop.
-# sudo find /roms2 -name '*.srm' -exec cp -t /roms2/backupsavs/ {} \;
-# printf "\n\nFinding .state files and copying them to backupsavs..."
-# sudo find /roms2 -name '*.state*' -exec cp -t /roms2/backupsavs/states/ {} \;
-# printf "\n\n\e[32mThe backup has been completed"
-sleep 5
-
-exit 0
+sleep 3
