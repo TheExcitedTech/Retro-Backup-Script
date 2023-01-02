dd2d4bd1e1213d60f79d87636d23bc826d5035b7
The Excited Tech <theexcitedtech@gmail.com>
2023-01-01 22:53:48 -0500

    Added a check for backup folder. Made a backup folder variable instead of hardcoding 'backupsavs.'

--- Backup_Saves.sh
+++ Backup_Saves.sh
@@ -28,27 +28,24 @@ sudo $CONTROLS Backup_Saves.sh rg552
 sleep 2
 
 SAVE_TYPES=("srm" "state*" "sav" "mcd")
-BACKUPFOLDER="roms2/backupsavs"
-TEMPFOLDER="roms2/temp_dir"
+BACKUPFOLDER="backupsavs"
+TEMPFOLDER="tmp/temp_svdir"
 
 BackUpSaves () {
 printf "\e[0mBacking up save files...\n"
 
-if [ ! -d "roms2/backupsavs" ]; then
-    sudo mkdir -v /roms2/backupsavs
-fi
 
 for svfile in ${SAVE_TYPES[@]}; do #creates subdirectories for each file type. 
     if [ $svfile == 'state*' ]; then #I don't think * will play nice with folder name. 
-        if [ ! -d "/roms2/backupsavs/state" ]; then
-            sudo mkdir -v /roms2/backupsavs/state
+        if [ ! -d "/roms2/$BACKUPFOLDER/state" ]; then
+            sudo mkdir -v /roms2/$BACKUPFOLDER/state
         fi
-        sudo find /roms2 -name "*.$svfile" -exec cp -t /roms2/backupsavs/state {} \;
-    elif [ ! -d "roms2/backupsavs/$svfile" ]; then
-        sudo mkdir -v /roms2/backupsavs/$svfile
+        sudo find /roms2 -name "*.$svfile" -exec cp -t /roms2/$BACKUPFOLDER/state {} \;
+    elif [ ! -d "roms2/$BACKUPFOLDER/$svfile" ]; then
+        sudo mkdir -v /roms2/$BACKUPFOLDER/$svfile
     fi
-printf "\n\nFinding $svfile files and copying them to backupsavs..."
-sudo find /roms2 -not -path */backupsavs/* -name "*.$svfile" -exec cp -f {} /roms2/backupsavs/$svfile \;
+printf "\n\nFinding $svfile files and copying them to $BACKUPFOLDER..."
+sudo find /roms2 -not -path */$BACKUPFOLDER/* -name "*.$svfile" -exec cp -f {} /roms2/$BACKUPFOLDER/$svfile \;
 done
 
 printf "\n\n\e[32mYour saves have been backed up"
@@ -56,18 +53,34 @@ sleep 3
 }
 
 CompareSavDirs () {
-    diff -qr $BACKUPFOLDER $TEMPFOLDER 
+    diff -qr roms2/$BACKUPFOLDER $TEMPFOLDER 
 }
 
-dialog --title "Warning" --yesno "This will overwrite any saves in the backupsavs folder. \n Do you want to continue?\nNote:If you don't have a backupsavs folder this will create it." $height $width
-if [ $? = 0 ]; then
+if [ ! -d "roms2/$BACKUPFOLDER" ]; then
+    sudo mkdir -v /roms2/$BACKUPFOLDER
     BackUpSaves
-    pgrep -f oga_controls | sudo xargs kill -9
-elif [ $? = 1 ]; then
-    printf "No action taken. Exiting Script..."
-    sleep 2
-    pgrep -f oga_controls | sudo xargs kill -9
-    exit 1
-fi
+else
+    dialog --title "Warning" --yesno "This will overwrite any saves in the $BACKUPFOLDER folder. \n Do you want to continue?\nNote:If you don't have a $BACKUPFOLDER folder this will create it." $height $width
+    response=$?
+    case $response in
+        0) BackUpSaves
+        pgrep -f oga_controls | sudo xargs kill -9;;
+   1) printf "No action taken. Exiting Script..."
+     sleep 2
+     pgrep -f oga_controls | sudo xargs kill -9
+     exit 1;;
+   255) echo "[ESC] key pressed.";;
+esac
+
+
+# if [ $? = 0 ]; then
+#     BackUpSaves
+#     pgrep -f oga_controls | sudo xargs kill -9
+# elif [ $? = 1 ]; then
+#     printf "No action taken. Exiting Script..."
+#     sleep 2
+#     pgrep -f oga_controls | sudo xargs kill -9
+#     exit 1
+# fi
 
 exit 0
